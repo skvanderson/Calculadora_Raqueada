a28402972a2ef1eac1bbfe4ac389503c5cee2927
Sharlles Andersonn <54425710+skvanderson@users.noreply.github.com>
2024-02-10 02:53:14 -0300

    Add files via upload

---
+++ Calculadora_Raqueada/desafio/index.js
@@ -0,0 +1,30 @@
+function calcularNivel(vitorias, derrotas) {
+   
+    let saldoVitorias = vitorias - derrotas;
+    let nivel = "";
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 10 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias > 20 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias > 50 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias > 80 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias > 90 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    // Retorna o resultado
+    return {
+        saldoVitorias: saldoVitorias,
+        nivel: nivel
+    };
+}
+
+let resultado = calcularNivel(85, 15);
+console.log("O Herói tem saldo de " + resultado.saldoVitorias + " está no nível de " + resultado.nivel);
